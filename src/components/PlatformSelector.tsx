4f7fa1038f7ff87c7d5873fa44bb6a6cf07d670e
Adrian Jelonek <adeq0990jelonek@gmail.com>
2024-05-03 01:11:12 +0200

    Refactor: Extract a query object

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -2,10 +2,9 @@ import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
 import { Platform } from "../hooks/useGames";
-
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
-  selectedPlatform: Platform;
+  selectedPlatform: Platform | null;
 }
 
 function PlatformSelector({ onSelectPlatform, selectedPlatform }: Props) {
