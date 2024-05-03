f96f3a90c53ec6ae4dc348cc7dd20a8ba20dec66
Adrian Jelonek <adeq0990jelonek@gmail.com>
2024-05-03 04:24:06 +0200

    Update page title

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,9 +1,7 @@
-import { Box, Card, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Card, HStack, Heading, Image, CardBody } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
-import { CardBody } from "react-bootstrap";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
-import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
 
 interface Props {
