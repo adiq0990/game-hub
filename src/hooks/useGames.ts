d6a06aeae52e825ca23028784b5f958675924448
Adrian Jelonek <adeq0990jelonek@gmail.com>
2024-05-04 02:53:36 +0200

    Refactor: fetch games with react query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,13 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
-import { Genre } from "./useGenres";
+import apiClient, { FetchResponse } from "../services/api-client";
 
 export interface Platform {
   id: number;
   name: string;
-  slug: string
+  slug: string;
 }
-
 export interface Game {
   id: number;
   name: string;
@@ -17,15 +16,20 @@ export interface Game {
   rating_top: number;
 }
 
-const useGames = (gameQuery: GameQuery) => useData<Game>('/games', 
-  {
-   params: 
-   {
-    genres: gameQuery.genre?.id, 
-    platforms: gameQuery.platform?.id,
-    ordering: gameQuery.sortOrder,
-    search: gameQuery.searchText
-  }},
-  [gameQuery])
+const useGames = (gameQuery: GameQuery) =>
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
 
 export default useGames;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-  baseURL: 'https://api.rawg.io/api',
+  baseURL: "https://api.rawg.io/api",
   params: {
-    key: '01291db0600846928d100a6e0274aec6'
-  }
-})
+    key: "01291db0600846928d100a6e0274aec6",
+  },
+});
